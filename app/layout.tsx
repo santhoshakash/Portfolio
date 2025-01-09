20071be59b18eb60782aa5e4cb4958eb8b57118c
santhoshakash <santhosh.akash@codem.com>
2025-01-09 12:49:08 +0530

    Tenth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import './globals.css';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -9,6 +10,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <Script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-F5GB46KQ9M'
+        ></Script>
+        <Script id='google-analytics'>
+          {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+
+           gtag('config', 'G-F5GB46KQ9M');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
